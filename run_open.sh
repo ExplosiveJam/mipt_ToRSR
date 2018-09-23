7a32079240accbc2108d25aaa485850fe2c1de59
Andrew Gonchar <biohazard100000@gmail.com>
2018-09-23 19:53:11 +0300

    Update run_open.sh

--- run_open.sh
+++ run_open.sh
@@ -1,5 +1,5 @@
 conda env create -f myenv.yml -n environment
 source activate environment
-jupyter nbconvert --to html hw1.ipynb
+jupyter nbconvert --to html task_0.ipynb
 google-chrome hw1.html
 source deactivate
