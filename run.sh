e34747f056bb69d7d58d3bc5f089bb982f9847db
Andrew Gonchar <biohazard100000@gmail.com>
2018-09-30 19:58:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 conda env create -f myenv.yml -n environment
 source activate environment
-jupyter nbconvert --to html task_0.ipynb
+jupyter nbconvert --execute --to html task_0.ipynb
 source deactivate
